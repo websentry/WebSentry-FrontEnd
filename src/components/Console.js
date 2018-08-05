7a0d02080849afd0798fe134c57a775e00094610
CaviarChen <yzj@vlabpro.com>
2018-08-05 22:02:09 +1000

    Add SentryList

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+
+
 import CommonPage from './CommonPage';
+import ConsoleSentryList from './Console/SentryList';
 
 
-const Console = () => (
+const Console = ({match}) => (
     <CommonPage pageType='Console'>
-        Console
+
+        <Route exact path={match.url} component={ConsoleSentryList}/>
     </CommonPage>
 )
+
+
 
 export default Console
