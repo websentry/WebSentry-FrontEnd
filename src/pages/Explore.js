c0a3a9e110eeb8018161c7c8a490888df1291795
zijies1 <546641464@qq.com>
2019-06-29 14:14:29 +1000

    including boostrap

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import AppLayout from '../layouts/AppLayout';
+import {
+  Layout,
+  Breadcrumb
+} from 'antd';
 
-class App extends Component {
+const { Header, Content, Footer } = Layout;
+
+class App extends React.Component {
   render() {
     return (
       <AppLayout>
-            <div>
-                hahahahahaha
+        <div class="container mt-5">
+          <div class="jumbotron">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          </div>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control form-control-lg" placeholder="Enter website: www.google.com" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+            <div class="input-group-append">
+              <button type="button" class="btn btn-primary btn-lg">Large button</button>
             </div>
-        </AppLayout>
+          </div>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control form-control-lg" placeholder="me@example.com" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+            <div class="input-group-append">
+              <button type="button" class="btn btn-primary btn-lg">Large button</button>
+            </div>
+          </div>
+        </div>
+
+      </AppLayout>
     );
   }
 }
