860bd2e5f71699c10d3fa6584057bcbab46612d8
zijies1 <546641464@qq.com>
2019-06-30 20:14:25 +1000

    include bootstrap in a better way

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
 
