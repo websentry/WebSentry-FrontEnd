d164d39635b141e0ed4f22d327a3c18bf035478a
zijies1 <546641464@qq.com>
2019-07-11 22:50:54 +1000

    delete unrelated content

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import AppLayout from '../layouts/AppLayout';
-import Banner from "./explore/Banner";
-import HomeSection from "./explore/HomeSection";
-import HomeHeading from "./explore/HomeHeading";
-
 import './Explore.less';
 
 class Explore extends React.Component {
@@ -11,25 +7,7 @@ class Explore extends React.Component {
   render() {
     return (
       <AppLayout>
-        <Banner/>
-        <HomeSection/>
-        <HomeHeading/>
-        <section id="newsletter" className="text-center p-5 bg-dark text-white">
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h1 className ="text-white">Sign Up For Our Newsletter</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio asperiores consectetur, quae ducimus voluptates
-                  vero repellendus architecto maiores recusandae mollitia?</p>
-                <form className="form-inline justify-content-center">
-                  <input type="text" className="form-control mb-2 mr-2" placeholder="Enter Name"/>
-                  <input type="text" className="form-control mb-2 mr-2" placeholder="Enter Email"/>
-                  <button className="btn btn-primary mb-2">Submit</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </section>
+        blank exlore page
       </AppLayout>
     );
   }
