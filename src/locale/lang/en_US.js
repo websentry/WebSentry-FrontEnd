dfc7fb536593554d3475de30fc3be17596439787
zijies1 <546641464@qq.com>
2019-07-13 21:03:47 +1000

    remove unused text

--- src/locale/lang/en_US.js
+++ src/locale/lang/en_US.js
@@ -4,12 +4,6 @@ const en_US = {
   dashboard: 'Dashboard',
   signIn:'Sign in',
   signUp:'Sign up',
-  signOut:'Sign out',
-  bannerHeader1:'Header One',
-  bannerMsg1:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam vel ullam deleniti reiciendis ratione quod aliquid inventore vero perspiciatis.',
-  bannerHeader2:'Header Two',
-  bannerMsg2:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam vel ullam deleniti reiciendis ratione quod aliquid inventore vero perspiciatis.',
-  bannerHeader3:'Header Three',
-  bannerMsg3:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam vel ullam deleniti reiciendis ratione quod aliquid inventore vero perspiciatis.'
+  signOut:'Sign out'
 }
 export default en_US;
--- src/locale/lang/zh_CN.js
+++ src/locale/lang/zh_CN.js
@@ -4,12 +4,6 @@ const zh_CN = {
   dashboard: '主页',
   signIn:'登陆',
   signUp:'注册',
-  signOut:'登出',
-  bannerHeader1:'标题一',
-  bannerMsg1:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam vel ullam deleniti reiciendis ratione quod aliquid inventore vero perspiciatis.',
-  bannerHeader2:'标题二',
-  bannerMsg2:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam vel ullam deleniti reiciendis ratione quod aliquid inventore vero perspiciatis.',
-  bannerHeader3:'标题三',
-  bannerMsg3:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam vel ullam deleniti reiciendis ratione quod aliquid inventore vero perspiciatis.'
+  signOut:'登出'
 }
 export default zh_CN;
