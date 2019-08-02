6ca42cc9ae7bcffd98a8b61b66a50df105cba127
zijies1 <546641464@qq.com>
2019-08-02 14:18:38 +1000

    add url error message

--- src/pages/dashboard/sentry/NewSentry.js
+++ src/pages/dashboard/sentry/NewSentry.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
-import { Input, Spin, Icon, Select, Col, Row, PageHeader, Divider, Button } from 'antd';
+import {  Input,
+          Spin,
+          Icon,
+          Select,
+          Col,
+          Row,
+          PageHeader,
+          Divider,
+          Button } from 'antd';
 import api from '../../../helpers/Api.js';
+import "./NewSentry.less";
 
 const { Search } = Input;
 const InputGroup = Input.Group;
 const { Option } = Select;
 const initialState = {
   error:null,
-  isFormLoading: false,     // form loading
+  urlError:null,
+  isFormLoading:false,     // form loading
   isUrlLoading:false,        // url loading
   okText:"Submit",
   okButtonProps:{
@@ -47,22 +57,28 @@ class NewSentry extends Component {
     this.onCancel = this.onCancel.bind(this);
   }
 
+  componentDidMount() {
+    if(this.props.notificationList[0] == null) {
+      this.setState({ error: "Failed to load notification list" });
+    } else {
+      this.setDefaultNotification();
+    }
+  }
+
   onSentryValueChange(key,val) {
     console.log(key,val);
     this.setState({[key]: val });
   }
 
-  setDefaultNotification(){
+  setDefaultNotification() {
     this.setState({ notification: this.props.notificationList[0].type });
     this.setState({ notificationId: this.props.notificationList[0]._id });
   }
 
-  componentDidMount() {
-    if(this.props.notificationList[0] == null){
-      this.setState({ error: "Failed to load notification list" });
-    }else{
-      this.setDefaultNotification();
-    }
+  onCancel() {
+    this.props.hideCreateSentry();
+    this.setState(initialState);
+    this.setDefaultNotification();
   }
 
   onImageLoaded = image => {
@@ -79,7 +95,7 @@ class NewSentry extends Component {
 
   async handleUrlSubmit(url,event) {
     event.preventDefault();
-    this.setState({ isUrlLoading: true });
+    this.setState({ isUrlLoading: true , urlError: null });
     let res = await api.requestFullScreenshot(this.state.url);
     console.log(res);
     if (res.code === api.code.ok) {
@@ -93,18 +109,16 @@ class NewSentry extends Component {
                 api.getFullScreenshotLink(sentryId, res.data.imageToken)
             }
           );
+        } else {
+          this.setState({ isUrlLoading: false, urlError: res.msg  });
         }
+    } else {
+      this.setState({ isUrlLoading: false, urlError: res.msg  });
     }
     this.setState({ isUrlLoading: false });
   }
 
-  onCancel(){
-    this.props.hideCreateSentry();
-    this.setState(initialState);
-    this.setDefaultNotification();
-  }
-
-  async handleSentrySubmit(){
+  async handleSentrySubmit() {
     const { name, url, notificationId } = this.state;
     let { x, y, width, height } = this.state.crop;
 
@@ -139,31 +153,27 @@ class NewSentry extends Component {
 
     if(isUrlLoading) {
       return(
-        <div className="mt-4">
-          <Row justify={"center"} type = {"flex"} align={"middle"}>
-             <Spin indicator={antIcon} size="large"/>
-          </Row>
-        </div>
+        <Row justify={"center"} type = {"flex"} align={"middle"}>
+           <Spin indicator={antIcon} size="large"/>
+        </Row>
       )
     }
     if(screenshotLink) {
       return(
-        <div className = "mt-3 mb-2">
-          <Row justify={"center"} type = {"flex"} align={"middle"}>
-            <ReactCrop
-              src={screenshotLink}
-              crop={crop}
-              onImageLoaded={this.onImageLoaded}
-              onComplete={this.props.onCropComplete}
-              onChange={this.onCropChange}
-              keepSelection={true}
-            />
-          </Row>
-        </div>
+        <Row justify={"center"} type = {"flex"} align={"middle"}>
+          <ReactCrop
+            src={screenshotLink}
+            crop={crop}
+            onImageLoaded={this.onImageLoaded}
+            onComplete={this.props.onCropComplete}
+            onChange={this.onCropChange}
+            keepSelection={true}
+          />
+        </Row>
       )
     }
     return(
-      <div className="jumbotron mt-3 fixedHeight mb-2">
+      <div className="jumbotron fixedHeight">
       </div>
     )
   }
@@ -172,29 +182,30 @@ class NewSentry extends Component {
   render() {
     return (
       <div>
-        <PageHeader
-          title="New Sentry"
-        />
+        <PageHeader title="New Sentry" />
         <Divider />
-        <div className="pl-7 pr-7">
+        <div className="pl-5 pr-5">
           <div className="mt-2">
             <Search
-               placeholder="Enter website: www.google.com"
-               enterButton="search"
+               placeholder="Enter website: https://www.google.com"
+               enterButton="Search"
                size="large"
                value={this.state.url}
                onChange={this.urlOnchange}
                onSearch={this.handleUrlSubmit}
             />
+            {this.state.urlError?<div className="red mt-1">{this.state.urlError}</div>:null}
           </div>
-          {this.renderCrop()}
+          <div className="mt-3 mb-3">
+            {this.renderCrop()}
+          </div>
           <InputGroup compact>
             <Input
               size="large"
               style={{ width: '70%' }}
               value={this.state.name}
               onChange={this.nameOnchange}
-              placeholder="Enter name: John"
+              placeholder="Enter sentry name: Google Banner"
             />
               <Select
                 size="large"
@@ -209,16 +220,23 @@ class NewSentry extends Component {
                     </Option>)
                 })}
               </Select>
-          </InputGroup>
-            {this.state.error?<div className="red-6 mt-1">{this.state.error}</div>:null}
+            </InputGroup>
+            {this.state.error?<div className="red mt-1">{this.state.error}</div>:null}
             <Row style={{ marginTop: 10 }}>
               <Col span={24} style={{ textAlign: 'center' }}>
-                <Button type="primary" onClick={this.handleSentrySubmit} size="large">
-                  Sumbit
+                <Button
+                  type="primary"
+                  onClick={this.handleSentrySubmit}
+                  loading={this.state.isFormLoading}
+                  size="large"
+                >
+                  Submit
                 </Button>
-                <Button style={{ marginLeft: 8 }} onClick={this.onCancel} size="large">
-                  Cancel
-                </Button>
+                {this.state.isFormLoading?null:
+                  <Button style={{ marginLeft: 8 }} onClick={this.onCancel} size="large">
+                    Cancel
+                  </Button>
+                }
               </Col>
             </Row>
           </div>
