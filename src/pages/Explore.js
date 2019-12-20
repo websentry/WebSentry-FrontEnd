6f17578deb549ce3704f5b3639cb16eb6ef9caf2
Mars Xue <marsxue0221@gmail.com>
2019-12-20 23:09:59 +0800

    Fix

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -7,7 +7,14 @@ class Explore extends React.Component {
   render() {
     return (
       <AppLayout>
-        blank exlore page
+        <div style={{ background: '#4190f7', padding: 24, minHeight: '100%', boxShadow: 'inset 0 0 5rem' }}>
+          <div style={{ color: '#ffffff', textAlign: 'center', fontSize: '64px', marginTop: '12%' }}>
+            WebSentry
+          </div>
+          <div style={{ color: '#ffffff', textAlign: 'center', fontSize: '32px', marginTop: '5%', marginLeft: '15%', marginRight: '15%' }}>
+            Web Sentry is an open-source web service for monitoring selected sections of web pages. It automatically notifies the user if any changes happen.
+          </div>
+        </div>
       </AppLayout>
     );
   }
