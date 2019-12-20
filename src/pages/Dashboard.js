14090e4d7a9228ce9d4e132ec7c18fec5458c761
Mars Xue <marsxue0221@gmail.com>
2019-12-20 17:57:16 +0800

    Modify Dashboard page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import { Spin } from 'antd';
 import Container from './dashboard/Container';
 import DashboardLayout from '../layouts/DashboardLayout';
+import AppLayout from '../layouts/AppLayout';
 import { UserContext } from '../UserContext';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Login from './Login';
 import './Dashboard.less';
 
 class Dashboard extends Component {
@@ -13,14 +12,21 @@ class Dashboard extends Component {
     if (isLoggedIn) {
       return (
         <DashboardLayout>
-          <Container/>
+          <Container />
         </DashboardLayout>
       )
     };
     return (
-      <Router>
-        <Route path="/dashboard" component={Login} /> 
-      </Router>
+      <AppLayout>
+        <div style={{ background: '#4190f7', padding: 24, minHeight: '100%' }}>
+          <div style={{ color: '#ffffff', textAlign: 'center', fontSize: '40px', marginTop: '15%' }}>
+            WebSentry
+          </div>
+          <div style={{ color: '#ffffff', textAlign: 'center', fontSize: '20px', marginTop: '5%', marginLeft: '20%', marginRight: '20%' }}>
+            Web Sentry is an open-source web service for monitoring selected sections of web pages. It automatically notifies the user if any changes happen.
+          </div>
+        </div>
+      </AppLayout>
     )
   }
 
