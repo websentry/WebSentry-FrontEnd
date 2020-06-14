88f43effd939eaa26da120d80df55ddfc82e11ed
Zed(Zijun) Chen <c.zijunchen@gmail.com>
2020-06-14 02:59:27 -0700

    Handle login loading state in AppHeader.

--- src/layouts/AppHeader.js
+++ src/layouts/AppHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { GlobalOutlined } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import { GlobalOutlined, LoadingOutlined } from '@ant-design/icons';
+import { Layout, Menu, Spin } from 'antd';
 import { FormattedMessage } from 'react-intl';
 import { UserContext } from '../UserContext';
 import { Link } from "react-router-dom";
@@ -14,116 +14,97 @@ class AppHeader extends React.Component {
   render() {
     return (
       <UserContext.Consumer>
-        {/* TODO check loading state */}
-        { ({switchLang, isLoggedIn}) => {
-            return (
-              <Header className="app-header">
-                <img src={logo} className="app-header-logo" alt=""/>
-                { isLoggedIn ?
-                  <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    style={{ lineHeight: "64px" }}
-                    defaultSelectedKeys={["home"]}
-                    selectedKeys={[this.props.selected]}
-                  >
-                    <Menu.Item
-                      key="home"
-                      style={{ width: "80px", textAlign: "center" }}
-                    >
-                      <Link to="/">
-                        <FormattedMessage
-                          id='home'
-                          defaultMessage='Home'
-                        />
-                      </Link>
-                    </Menu.Item>
-                    <SubMenu
-                      key="language"
-                      title={
-                        <span>
-                          <GlobalOutlined style={{ fontSize: "16px" }} />
-                        </span>
-                      }
-                      style={{ float: "right" }}
-                    >
-                      <Menu.Item key="switchLang" onClick={switchLang}>
-                        <FormattedMessage
-                          id='lang'
-                          defaultMessage='Language:English'
-                        />
-                      </Menu.Item>
-                    </SubMenu>
-                    <Menu.Item key="dashboard" style={{ float: "right" }}>
-                      <Link to="/dashboard">
-                        <FormattedMessage
-                          id='dashboard'
-                          defaultMessage='Dashboard'
-                        />
-                      </Link>
-                    </Menu.Item>
-                  </Menu>
-                  :
-                  <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    style={{ lineHeight: "64px" }}
-                    defaultSelectedKeys={["home"]}
-                    selectedKeys={[this.props.selected]}
-                  >
-                    <Menu.Item
-                      key="home"
-                      style={{ width: "80px", textAlign: "center" }}
-                    >
-                      <Link to="/">
-                        <FormattedMessage
-                          id='home'
-                          defaultMessage='Home'
-                        />
-                      </Link>
-                    </Menu.Item>
-                    <SubMenu
-                      key="language"
-                      title={
-                        <span>
-                          <GlobalOutlined style={{ fontSize: "16px" }} />
-                        </span>
-                      }
-                      style={{ float: "right" }}
-                    >
-                      <Menu.Item key="switchLang" onClick={switchLang}>
-                        <FormattedMessage
-                          id='lang'
-                          defaultMessage='Language:English'
-                        />
-                      </Menu.Item>
-                    </SubMenu>
-                    <Menu.Item
-                      key="register"
-                      style={{ width: "80px", textAlign: "center", float: "right" }}
-                    >
-                      <Link to='/register'>
-                        <FormattedMessage
-                          id='signUp'
-                          defaultMessage='Sign up'
-                        />
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item 
-                      key="login"
-                      style={{ width: "80px", textAlign: "center", float: "right" }}
-                    >
-                      <Link to="/login">
-                        <FormattedMessage
-                          id='signIn'
-                          defaultMessage='Sign in'
-                        />
-                      </Link>
-                    </Menu.Item>
-                  </Menu>
-                }
-              </Header>
-            );
+        {({ switchLang, isLoggedIn, isLoading }) => {
+          let userMenu;
+          if (isLoading) {
+            const spinIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+            userMenu = (
+              <Menu.Item key="dashboard" style={{ float: "right" }}>
+                <Spin indicator={spinIcon} />
+              </Menu.Item>
+            )
+          } else {
+            if (isLoggedIn) {
+              userMenu = (
+                <Menu.Item key="dashboard" style={{ float: "right" }}>
+                  <Link to="/dashboard">
+                    <FormattedMessage
+                      id='dashboard'
+                      defaultMessage='Dashboard'
+                    />
+                  </Link>
+                </Menu.Item>
+              )
+            } else {
+              userMenu = [
+                <Menu.Item
+                  key="register"
+                  style={{ width: "80px", textAlign: "center", float: "right" }}
+                >
+                  <Link to='/register'>
+                    <FormattedMessage
+                      id='signUp'
+                      defaultMessage='Sign up'
+                    />
+                  </Link>
+                </Menu.Item>
+                ,
+                <Menu.Item
+                  key="login"
+                  style={{ width: "80px", textAlign: "center", float: "right" }}
+                >
+                  <Link to="/login">
+                    <FormattedMessage
+                      id='signIn'
+                      defaultMessage='Sign in'
+                    />
+                  </Link>
+                </Menu.Item>
+              ]
+            }
+          }
+
+          return (
+            <Header className="app-header">
+              <img src={logo} className="app-header-logo" alt="" />
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                style={{ lineHeight: "64px" }}
+                defaultSelectedKeys={["home"]}
+                selectedKeys={[this.props.selected]}
+              >
+                <Menu.Item
+                  key="home"
+                  style={{ width: "80px", textAlign: "center" }}
+                >
+                  <Link to="/">
+                    <FormattedMessage
+                      id='home'
+                      defaultMessage='Home'
+                    />
+                  </Link>
+                </Menu.Item>
+                <SubMenu
+                  key="language"
+                  title={
+                    <span>
+                      <GlobalOutlined style={{ fontSize: "16px" }} />
+                    </span>
+                  }
+                  style={{ float: "right" }}
+                >
+                  <Menu.Item key="switchLang" onClick={switchLang}>
+                    <FormattedMessage
+                      id='lang'
+                      defaultMessage='Language:English'
+                    />
+                  </Menu.Item>
+                </SubMenu>
+                {userMenu}
+              </Menu>
+            </Header>
+          );
         }}
       </UserContext.Consumer>
     );
