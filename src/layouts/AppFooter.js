c4f5e49204bb455ac81d6a17e3250c2c42658a74
Mars Xue <marsxue0221@gmail.com>
2020-09-05 19:37:36 +0800

    Update

--- src/layouts/AppFooter.js
+++ src/layouts/AppFooter.js
@@ -11,7 +11,11 @@ class AppFooter extends React.Component {
       <Footer className="app-footer">
         Copyright <CopyrightOutlined /> WebSentry
         <br />
-        <a href="https://github.com/websentry/WebSentry">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/websentry/WebSentry"
+        >
           View on Github <GithubOutlined />
         </a>
       </Footer>
